9ad8b09eb70a3a77b752485ca03ed565767e7420
David Vargas <dvargas92495@gmail.com>
2022-05-23 23:38:06 -0600

    Initial commit from Remix Fuego Stack

---
+++ README.md
@@ -0,0 +1,5 @@
+# parlaypartay.davidvargas.me
+
+Accessible at [https://parlaypartay.davidvargas.me](https://parlaypartay.davidvargas.me). 
+
+Description for parlaypartay.davidvargas.me.
---
+++ app/routes/__public/privacy-policy.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import getMeta from "@dvargas92495/app/utils/getMeta";
+import PrivacyPolicy from "@dvargas92495/app/components/PrivacyPolicy";
+
+const PrivacyPolicyPage: React.FunctionComponent = () => (
+  <PrivacyPolicy name={"Parlaypartay"} domain={"davidvargas.me"} />
+);
+
+export const Head = getMeta({ title: "Privacy Policy" });
+
+export default PrivacyPolicyPage;
