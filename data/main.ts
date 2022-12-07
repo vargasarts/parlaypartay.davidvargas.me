f0ecad236619bfd10c436decda3009a43187c382
David Vargas <dvargas92495@gmail.com>
2022-12-07 15:28:57 -0500

    deploy database_url secret

--- data/main.ts
+++ data/main.ts
@@ -3,6 +3,6 @@ import schema from "./schema";
 
 base({
   projectName: "parlaypartay.davidvargas.me",
-  variables: ["odds_api_key", "clerk_api_key"],
+  variables: ["odds_api_key", "clerk_api_key", "database_url"],
   schema,
 });
